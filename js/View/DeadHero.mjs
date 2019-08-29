1a11751ee00509684bcb0d915cd21da648555fe9
AZ <agnieszkayd@gmail.com>
2019-08-29 15:37:19 +0200

    refactoring of common code in views into utility method

--- js/View/DeadHero.mjs
+++ js/View/DeadHero.mjs
@@ -1,26 +1,10 @@
 'use script';
 
 import { MiscUtils } from '../Util/MiscUtils.mjs';
+import { POSSIBLE_DEATH_OPTIONS } from '../const.mjs';
 
 export class DeadHero {
     static show( dom, callback ) {
-        MiscUtils.domClear( dom );
-
-        const btn = document.createElement( 'A' );
-        btn.href = '#';
-        btn.style.display = 'block';
-        btn.innerText = 'Restart?';
-        const listener = ( evt => {
-            callback( evt ); // note: ATM this is completely redundant!
-        } );
-        btn.addEventListener( 'click', listener );
-        dom.appendChild( btn );
-
-        window.onkeyup = function ( evt ) {
-            if ( evt.key === 'r' ) {
-                window.onkeyup = null;
-                listener( evt );
-            }
-        };
+        MiscUtils.showMenu( dom, POSSIBLE_DEATH_OPTIONS, null, callback );
     }
 }
--- js/View/Fight.mjs
+++ js/View/Fight.mjs
@@ -5,33 +5,6 @@ import { POSSIBLE_COMBAT_ACTIONS } from '../const.mjs';
 
 export class Fight {
     static show( dom, callback ) {
-        MiscUtils.domClear( dom );
-
-        const p = document.createElement( 'P' );
-        p.innerText = 'Choose action:';
-        dom.appendChild( p );
-
-        const keyMap = new Map();
-        POSSIBLE_COMBAT_ACTIONS.forEach( x => {
-            const btn = document.createElement( 'A' );
-            btn.href = '#';
-            btn.style.display = 'block';
-            btn.innerText = x;
-            const listener = ( evt => {
-                callback( x, evt );
-            } );
-            btn.addEventListener( 'click', listener );
-            keyMap.set( x.charAt( 0 ).toLowerCase(), listener );
-            dom.appendChild( btn );
-        } );
-
-        window.onkeyup = function ( evt ) {
-            const listener = keyMap.get( evt.key );
-
-            if ( listener !== undefined ) {
-                //window.onkeyup = null;
-                listener( evt );
-            }
-        };
+        MiscUtils.showMenu( dom, POSSIBLE_COMBAT_ACTIONS, 'Choose action:', callback );
     }
 }
